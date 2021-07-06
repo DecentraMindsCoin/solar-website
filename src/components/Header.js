d364cb437ac22be8a58264e24a10192d4b79be7d
DecentraMindsCoin <decentraminds@gmail.com>
2021-07-06 01:11:24 -0700

    initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import Image from "next/image";
+
+function Header() {
+  return (
+    <header className="" >
+      <Image
+        src="/meraki.png"
+        width={150}
+        height={40}
+        objectFit="contain"
+        className="cursor-pointer"
+      />
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
